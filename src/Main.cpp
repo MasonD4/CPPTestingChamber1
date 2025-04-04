82227ae2eceb4b88bd181ef925d9a104ce09715b
MasonD4 <mcdunn@zonemail.clpccd.edu>
2025-04-04 11:03:30 -0700

    Added the Airship class, as well as constructor messages

--- src/Main.cpp
+++ src/Main.cpp
@@ -4,7 +4,7 @@
  *  Created on: Mar 21, 2025
  *      Author: Mason Dunn
  * 
- * Most recent commit: Fixed capitalization on Ship class (ship became Ship)
+ * 
  */
 
 #include <iostream>
@@ -24,11 +24,13 @@ public:
 		name = "Stock Ship";
 		size = "small";
 		speed = 1;
+		cout << "This is the default Ship constructor.\nName: " << name << endl;
 	}
 	Ship(string n, string si, int sp) {
 		name = n;
 		size = si;
 		speed = sp;
+		cout << "This is the <string, string, int> Ship constructor.\nName: " << name << endl;
 	}
 
 	string getName() {
@@ -50,7 +52,35 @@ public:
 	}
 };
 
+class Airship : public Ship {
+protected:
+	int altitude = 0;
+
+	public:
+	Airship() {
+		name = "Stock Airship";
+		size = "small";
+		speed = 1;
+		cout << "This is the default Airship constructor.\nName: " << name << endl;
+	}
+	Airship(string n, string si, int sp) {
+		name = "Stock Airship";
+		size = "small";
+		speed = 1;
+		cout << "This is the <string, string, int> Airship constructor.\nName: " << name << endl;
+	}
+
+	void fly(int a) {
+		altitude += a;
+		if (a < 0) {
+			a *= -1;
+		}
+		distanceTraveled += a;
+	}
+};
+
 int main() {
 	Ship boat("The Boat", "Huge", 5);
+	Airship melonBird("Melon-Bird", "Large", 10);
 	return 0;
 }
