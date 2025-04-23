49f583d9c2f8710d40abad737322132d72d22ea1
MasonD4 <mcdunn@zonemail.clpccd.edu>
2025-04-23 07:56:02 -0700

    Made an abstract Vehicle class, from which I derived a Car class and an Airship class. Then, I overloaded the plus operator to work on Car and Airship.

--- src/Main.cpp
+++ src/Main.cpp
@@ -13,91 +13,110 @@ using namespace std;
 class Vehicle {
 protected:
 	string name;
-	int xPosition;
+	int speed;
+
 public:
-	Vehicle () {
-		name = "Unnamed vehicle";
-		xPosition = 0;
+	Vehicle() {
+		name = "John Doe's Vehicle";
+		speed = 5;
 	}
-	Vehicle (string n) {
+	Vehicle(string n, int s) {
 		name = n;
-		xPosition = 0;
+		speed = s;
 	}
-	string getName() {
+	string getName(){
 		return name;
 	}
-	int getXPos() {
-		return xPosition;
+	int getSpeed() {
+		return speed;
 	}
-	void move(int m) {
-		xPosition += m;
+	virtual void move() = 0;
+};
+
+class Car : public Vehicle {
+	protected:
+	string licensePlate; // 7 digits
+	string color;
+	public:
+	Car() {
+		name = "John Doe's Car";
+		speed = 5;
+		licensePlate =  "1234567";
+		color = "Red";
 	}
-	virtual void whatAreYou() {
-		cout << "I am a Vehicle" << endl;
+	Car(string n, int s, string lP, string c) {
+		name = n;
+		speed = s;
+		licensePlate = lP;
+		color = c;
+	}
+	string getPlate() {
+		return licensePlate;
+	}
+	string getColor() {
+		return color;
+	}
+	void move() override {
+		cout << name << " is moving! It moved " << speed << " units." << endl;
 	}
 };
 
 class Airship : public Vehicle {
-private:
-	int yPosition;
+protected:
+	int altitude;
+	string purpose;
 public:
-	Airship () {
-		name = "Unnamed airship";
-		xPosition = 0;
-		yPosition = 0;
+	Airship() {
+		name = "John Doe's Airship";
+		speed = 5;
+		altitude = 0;
+		purpose = "Travel";
 	}
-	Airship (string n) {
+	Airship(string n, int s, int a, string p) {
 		name = n;
-		xPosition = 0;
-		yPosition = 0;
+		speed = s;
+		altitude = a;
+		purpose = p;
 	}
-	int getYPos() {
-		return yPosition;
+	int getAltitude(){
+		return altitude;
 	}
-	void fly(int f) {
-		yPosition += f;
+	string getPurpose() {
+		return purpose;
 	}
-	void whatAreYou() override {
-		cout << "I am an airship" << endl;
+	void move() override {
+		cout << name << " is moving! It moved " << speed << " units." << endl;
+	}
+	void fly(int a) {
+		altitude += a;
+		cout << name << " is flying! It moved " << a << " vertical units." << endl;
 	}
 };
 
-void RACE(Vehicle a, Vehicle b) {
-	if (a.getXPos() > b.getXPos()) {
-		cout << a.getName() << " won!" << endl;
-	}
-	else if (b.getXPos() > a.getXPos()) {
-		cout << b.getName() << " won!" << endl;
-	}
-	else {
-		cout << "TIE!" << endl;
-	}
+Car operator+(Car a, Car b) {
+	Car output(a.getName(), (a.getSpeed() + b.getSpeed()), b.getPlate(), b.getColor());
+	return output;
+}
+
+Airship operator+(Airship a, Airship b) {
+	Airship output(a.getName(), (a.getSpeed() + b.getSpeed()), (a.getAltitude() + b.getAltitude()), b.getPurpose());
+	return output;
 }
 
 int main() {
-	// Basically ` Base *base = new Derived `
-	Vehicle *melonBird = new Airship("Melon Bird");
-	melonBird->move(458);
+	Car johnsCar;
+	Car janesCar("Jane's Speed Car", 100, "LostD0g", "Green");
+	Car epicMobile =  johnsCar + janesCar;
 	
-	Airship johnsShip;
-	johnsShip.move(10); 
-	johnsShip.fly(5);
+	Airship melonBird("Melon-Bird", 458, 5480, "Transport and cargo");
+	Airship halberd("The Halberd", 500, 5099, "Aerial Battleship/ Flagship");
+	Airship theFlyingJohn;
+	Airship melonberd = melonBird + halberd;
+	
+	cout << melonberd.getName() << endl;
+	cout << melonberd.getSpeed() << endl;
+	cout << melonberd.getAltitude() << endl;
+	cout << melonberd.getPurpose() << endl;
 
-	Airship Halberd("The Battleship Halberd");
-	Halberd.fly(55);
-	Halberd.move(1000);
-
-	Vehicle *ptrToHalberd = &Halberd;
-	ptrToHalberd->move(15);
-
-	Vehicle janesCar;
-	janesCar.move(88);
-
-	RACE(janesCar, *melonBird);
-	RACE(Halberd, johnsShip);
-	RACE(janesCar, *ptrToHalberd);
-	cout << "Halberd: "; Halberd.whatAreYou();
-	cout << "Jane's car: "; janesCar.whatAreYou();
-	cout << "Melon Bird: "; melonBird->whatAreYou();
 	return 0;
 }
